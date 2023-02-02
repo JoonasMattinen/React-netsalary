2e2d73da051d100050808e3d1c0653faff949203
JoonasMattinen <N2majo02.students.oamk.fi>
2023-02-02 09:01:19 +0200

    added html form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-
 import './App.css';
 
 function App() {
   return (
-    <>
-    <p>testi</p>
-    </>
+    <div>
+      <form action="">
+        <h3>Laske nettopalkka</h3>
+        <div>
+          <label>Nettopalkka</label>
+          <input type="number" />
+        </div>
+        <div>
+          <label>Vero</label>
+          <input type="number" />
+        </div>
+        <div>
+          <label>Työeläke</label>
+          <select>
+            <option>0.25%</option>
+            <option>0.50%</option>
+            <option>0.75%</option>
+            <option>1.00%</option>
+          </select>
+        </div>
+        <div>
+          <label>Työttömyysvakuutus</label>
+          <select>
+            <option>0.25%</option>
+            <option>0.50%</option>
+            <option>0.75%</option>
+            <option>1.00%</option>
+          </select>
+        </div>
+        <div>
+          <label>Nettopalkka</label>
+          <output></output>
+        </div>
+        <button>Laske</button>
+      </form>
+
+    </div>
   );
 }
 
