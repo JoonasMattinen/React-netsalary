37e09f4f574d066197019a86076d1cf145ae6f5b
JoonasMattinen <N2majo02.students.oamk.fi>
2023-02-02 09:53:36 +0200

    added state variables

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
+import { useState } from 'react';
 import './App.css';
 import Options from './components/Options';
 
+
 function App() {
+
+  const [salary, setSalary] = useState("")
+  const [tax, setTax] = useState(0)
+  const [pension, setPension] = useState(0)
+  const [insurance, setInsurance] = useState(0)
+  const [netSalary, setNetSalary] = useState(0)
+
+  const [taxPayment, setTaxPayment] = useState(0)
+  const [pensionPayment, setPensionPayment] = useState(0)
+  const [insurancePayment, setInsurancePayment] = useState(0)
+
+  function calculateTax(value){
+    setTax(value)
+    setTaxPayment(salary / 100 * value)
+  }
+
   return (
     <div>
       <form action="">
         <h3>Laske nettopalkka</h3>
         <div>
-          <label>Nettopalkka</label>
-          <input type="number" />
+          <label>Bruttopalkka</label>
+          <input type="number" value={salary} onChange={e => setSalary(e.target.value)} />
         </div>
         <div>
           <label>Vero</label>
-          <input type="number" />
+          <input type="number" value={tax} onChange={e => calculateTax(e.target.value)}/>
+          <output>{taxPayment}€</output>
         </div>
         <div>
           <label>Työeläke</label>
